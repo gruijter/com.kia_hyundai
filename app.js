2403e2f242fdfe92022623d2c913e6924056fba2
gruijter <gruijter@hotmail.com>
2026-08-04 16:14:14 +0200

    Add/update GPL copyright headers, unify project name to com.kia_hyundai
    Adds the full copyright notice to every .js file created today that
    was missing one (lib/connect/**, lib/nomatim.js, lib/temp_convert.js,
    scripts/test-eu-connect.js). Updates the year to 2020 - 2026 and the
    project name to com.kia_hyundai everywhere, including in files that
    already had a header (device.js, driver.js, app.js) and lib/nomatim.js,
    which still referenced the old fork name com.gruijter.hyundai_kia and
    a different author email.

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /*
-Copyright 2025, RM de Gruijter (rmdegruijter@gmail.com)
+Copyright 2020 - 2026, RM de Gruijter (rmdegruijter@gmail.com)
 
-This file is part of com.kia and com.hyundai
+This file is part of com.kia_hyundai
 
-com.kia is free software: you can redistribute it and/or modify
+com.kia_hyundai is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-com.kia is distributed in the hope that it will be useful,
+com.kia_hyundai is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with com.kia. If not, see <http://www.gnu.org/licenses/>.
+along with com.kia_hyundai. If not, see <http://www.gnu.org/licenses/>.
 */
 
 'use strict';
